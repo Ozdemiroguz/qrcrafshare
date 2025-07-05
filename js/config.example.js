1d9264de58ecf68fe1a257f5fdeeecab3c3e11ed
Oguzhan <ozdemiroguzhan55@gmail.com>
2025-07-05 19:08:03 +0300

    Initial commit: Project setup and all pages

---
+++ js/config.example.js
@@ -0,0 +1,14 @@
+// This is an example file.
+// Create a new file named `config.js` in this same `js` directory.
+// Copy the contents of this file into `config.js` and fill in your actual Firebase project keys.
+// The `config.js` file is already in .gitignore, so your keys will be safe.
+
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+}; 
